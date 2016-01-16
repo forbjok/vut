70b69fdcc6d2827d1904c3438d915ae351393a7c
Forb.Jok <forbjok@gmail.com>
2016-01-16 02:32:54 +0100

    feat: Implement "init" command

--- source/commands/initcommand.d
+++ source/commands/initcommand.d
@@ -1,8 +1,10 @@
 import std.stdio;
 import std.file;
+import std.path;
 
 import command;
 import filelocator;
+import semver;
 
 class InitCommand : ICommand {
     static this() {
@@ -10,14 +12,35 @@ class InitCommand : ICommand {
     }
 
     int Execute(string[] args) {
-        writeln("Init!");
+        SemanticVersion semanticVersion = void;
+        if (args.length > 0) {
+            // If a version was specified on the commandline, parse it
+            auto versionString = args[0];
 
-        auto configFile = locateFileInPathOrParent(getcwd(), "VERSION");
+            semanticVersion = parseSemanticVersion(versionString);
+            if (semanticVersion is null) {
+                writefln("Invalid version: %s", versionString);
+                return 1;
+            }
+        }
+        else {
+            // If no version was specified, use default version
+            semanticVersion = new SemanticVersion(0, 0, 0);
+        }
 
-        if (configFile == null) {
-            writeln("No version file found.");
+        auto versionFile = locateFileInPathOrParent(getcwd(), "VERSION");
+
+        if (versionFile != null) {
+            writefln("An existing version file was found at: %s", versionFile);
             return 1;
         }
+
+        auto newVersionString = semanticVersion.toString();
+
+        // Write new version to file
+        std.file.write(buildPath(getcwd(), "VERSION"), cast(void[]) newVersionString);
+
+        writefln("Version initialized to %s.", newVersionString);
 
         return 0;
     }
