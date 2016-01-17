9c3ff1132591b1a9bac6ecc734113bc763286e2c
Forb.Jok <forbjok@gmail.com>
2016-01-17 18:14:22 +0100

    feat: Parse commandline args in main()

--- source/app.d
+++ source/app.d
@@ -1,15 +1,40 @@
+import std.algorithm;
+import std.array;
+import std.range;
 import std.stdio;
-import std.format;
-import std.file;
+import std.getopt;
+import std.path;
 
 import commands;
-import filelocator;
 
 int main(string[] args)
 {
+    try {
+        /* Concatenate the executable path with all subsequent arguments up to
+           the first one that does not start with optionChar. (normally "-")
+
+           This is required to prevent getopt from parsing options that are
+           intended for the command. */
+        auto options = chain(args.takeExactly(1), args[1..$].until!(a => !a.startsWith(optionChar))).array();
+
+        // Parse arguments
+        auto getoptResult = getopt(options);
+
+        if (getoptResult.helpWanted) {
+            // If user wants help, give it to them
+            writeUsage(args[0]);
+            return 0;
+        }
+    }
+    catch(Exception ex) {
+        // If there is an error parsing arguments, print it
+        writeln(ex.msg);
+        return 1;
+    }
+
     if (args.length == 1) {
         writeln("No command specified.");
-        printUsage();
+        writeUsage(args[0]);
         return 1;
     }
 
@@ -19,13 +44,13 @@ int main(string[] args)
     auto commandImplementation = getCommand(command);
     if (commandImplementation is null) {
         stderr.writefln("Unknown command: %s.", command);
-        printUsage();
+        writeUsage(args[0]);
         return 1;
     }
 
     return commandImplementation.Execute(args[1..$]);
 }
 
-void printUsage() {
-    writeln("Usage: vut <init|set|get|bump|generate> [--help] [...]");
+void writeUsage(in string executable) {
+    writefln("Usage: %s <init|set|get|bump|generate> [--help] [...]", executable.baseName());
 }
