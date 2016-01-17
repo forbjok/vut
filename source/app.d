abb867f13ca74712f022843bbee33a7e4bfc4e09
Forb.Jok <forbjok@gmail.com>
2016-01-17 02:01:17 +0100

    fix: Make usage information more helpful

--- source/app.d
+++ source/app.d
@@ -7,20 +7,25 @@ import filelocator;
 
 int main(string[] args)
 {
-	if (args.length == 1) {
-		writeln("No command specified.");
-		writeln("Usage: vut <command> [...]");
-		return 1;
-	}
+    if (args.length == 1) {
+        writeln("No command specified.");
+        printUsage();
+        return 1;
+    }
 
-	// Get command
-	auto command = args[1];
+    // Get command
+    auto command = args[1];
 
-	auto commandImplementation = getCommand(command);
-	if (commandImplementation is null) {
-		stderr.writefln("Unknown command '%s'.", command);
-		return 1;
-	}
+    auto commandImplementation = getCommand(command);
+    if (commandImplementation is null) {
+        stderr.writefln("Unknown command: %s.", command);
+        printUsage();
+        return 1;
+    }
 
-	return commandImplementation.Execute(args[1..$]);
+    return commandImplementation.Execute(args[1..$]);
+}
+
+void printUsage() {
+    writeln("Usage: vut <init|set|get|bump|generate> [--help] [...]");
 }
