d4597dd65fd7ac2727fb8bf895cdc0d5dc1d5203
Forb.Jok <forbjok@gmail.com>
2020-02-13 23:16:52 +0100

    chore: Update Windows build script + Chocolatey nuspec

--- build.cmd
+++ build.cmd
@@ -1,3 +1,16 @@
 @echo off
-dub build -b release
-choco pack chocolatey\package.nuspec
+setlocal
+
+rem Delete old "dist" directory
+rmdir /S /Q "dist"
+
+rem Build 32-bit executable for maximum OS support
+cargo build --release --target i686-pc-windows-msvc
+
+rem Create "dist" directory
+mkdir "dist"
+
+rem Build Chocolatey package
+choco pack chocolatey\package.nuspec --outputdirectory "dist"
+
+endlocal
