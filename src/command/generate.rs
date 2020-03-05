67119722109a69215a3068be182d7fea458a534c
Forb.Jok <forbjok@gmail.com>
2020-03-05 20:11:33 +0100

    feat: Change generating message

--- src/command/generate.rs
+++ src/command/generate.rs
@@ -5,7 +5,7 @@ use super::{stderr_vut_callbacks, CommandError};
 pub fn generate() -> Result<(), CommandError> {
     let vut = Vut::from_current_dir(Some(stderr_vut_callbacks()))?;
 
-    eprint!("Generating templates... ");
+    eprint!("Generating output... ");
 
     vut.generate_output()?;
 
