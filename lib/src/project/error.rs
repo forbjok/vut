553ac43e2067dc0b5787d969471e346fc6b19c16
Forb.Jok <forbjok@gmail.com>
2020-11-30 19:56:19 +0100

    refactor: Use thiserror in lib

--- lib/src/project/error.rs
+++ lib/src/project/error.rs
@@ -2,21 +2,37 @@ use std::borrow::Cow;
 use std::io;
 use std::path::PathBuf;
 
+use thiserror::Error;
+
 use crate::template::RenderTemplateError;
 use crate::util;
 
+#[derive(Debug, Error)]
 pub enum VutError {
+    #[error("An existing configuration was found at: {0}")]
     AlreadyInit(PathBuf),
+    #[error("Error opening config")]
     OpenConfig(util::FileError),
+    #[error("Error reading config")]
     ReadConfig(io::Error),
+    #[error("Error parsing config")]
     ParseConfig(Cow<'static, str>),
+    #[error("Configuration error")]
     Config(Cow<'static, str>),
+    #[error("Error writing config")]
     WriteConfig(io::Error),
+    #[error("No version source found")]
     NoVersionSource,
+    #[error("No version found in version source")]
     VersionNotFound,
+    #[error("Error opening version source")]
     VersionFileOpen(util::FileError),
+    #[error("Error reading version source")]
     VersionFileRead(io::Error),
+    #[error("Error writing to version source")]
     VersionFileWrite(io::Error),
+    #[error("Error generating template")]
     TemplateGenerate(RenderTemplateError),
+    #[error("Error")]
     Other(Cow<'static, str>),
 }
