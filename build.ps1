71b2e58591d31a17c56e90f3fade227438ba6644
Forb.Jok <forbjok@gmail.com>
2021-11-11 19:46:46 +0100

    feat: Chocolatey package improvements

--- build.ps1
+++ build.ps1
@@ -14,7 +14,14 @@ Write-Host "TargetDir: $TargetDir"
 function Compress-Target([string] $ArchiveName, [string] $Target) {
   Push-Location (Join-Path $TargetDir "$Target\release")
   try {
-    7z a -mx9 "$DistDir\$ArchiveName.zip" "vut.exe"
+    $ArchivePath = "$DistDir\$ArchiveName.zip"
+
+    # Compress archive
+    7z a -mx9 $ArchivePath "vut.exe"
+
+    # Generate checksum for archive
+    $checksum = (Get-FileHash -Path $ArchivePath -Algorithm SHA256).Hash.ToString()
+    Set-Content -Path "$ArchivePath.sha256.txt" -Encoding utf8NoBOM -Value $checksum -NoNewline
   } finally {
     Pop-Location
   }
@@ -44,5 +51,11 @@ $Version = (& $VutExe get json | ConvertFrom-Json).FullVersion
 Compress-Target "vut-$Version-windows-i686" "i686-pc-windows-msvc"
 Compress-Target "vut-$Version-windows-x86_64" "x86_64-pc-windows-msvc"
 
+# Write version file
+Set-Content -Path "$DistDir\VERSION.txt" -Encoding utf8NoBOM -Value $Version -NoNewline
+
 # Build Chocolatey package
-choco pack "chocolatey\package.nuspec" --outputdirectory "$DistDir"
+choco pack "chocolatey\package.nuspec" --outputdirectory "$DistDir" --version=$Version
+
+# Remove version file after building Chocolatey package
+Remove-Item -Path "$DistDir\VERSION.txt"
--- chocolatey/tools/ChocolateyInstall.ps1
+++ chocolatey/tools/ChocolateyInstall.ps1
@@ -1,13 +1,21 @@
+$toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
 $packageName = 'vut'
-$version = '0.1.2'
+$version = Get-Content -Path "$toolsPath\VERSION.txt" -Encoding utf8
 $url = "https://github.com/forbjok/vut/releases/download/v$version/vut-$version-windows-i686.zip"
 $url64 = "https://github.com/forbjok/vut/releases/download/v$version/vut-$version-windows-x86_64.zip"
-$unzipLocation = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$checksum = Get-Content -Path "$toolsPath\vut-$version-windows-i686.zip.sha256.txt" -Encoding utf8
+$checksum64 = Get-Content -Path "$toolsPath\vut-$version-windows-x86_64.zip.sha256.txt" -Encoding utf8
+$unzipLocation = $toolsPath
 
 $packageArgs = @{
   packageName = $packageName
   url = $url
   url64bit = $url64
+  checksum = $checksum
+  checksumType = "SHA256"
+  checksum64 = $checksum64
+  checksum64Type = "SHA256"
   unzipLocation = $unzipLocation
 }
 
