b8f6034f364e9f211d4025c1fa276077fbc74425
Forb.Jok <forbjok@gmail.com>
2022-02-23 17:53:49 +0100

    chore: Improve build script

--- build.ps1
+++ build.ps1
@@ -1,3 +1,14 @@
+function Cmd([string] $What, [scriptblock] $Action) {
+  $LASTEXITCODE = 0
+
+  Write-Host "--- $What ---" -ForegroundColor Green
+  & $Action
+
+  if ($LASTEXITCODE -ne 0) {
+    throw "$What failed with exit code $LASTEXITCODE."
+  }
+}
+
 <# Delete a file or directory and all its conents #>
 function Remove-Dir([string] $Path) {
   if (Test-Path $Path) {
@@ -5,57 +16,68 @@ function Remove-Dir([string] $Path) {
   }
 }
 
-$DistDir = Join-Path $PSScriptRoot "dist"
-$TargetDir = Join-Path $PSScriptRoot "target"
+# Variables
+$root = $PSScriptRoot
+$distPath = "$root\dist"
+$stagingRoot = "$root\staging"
+$executableName = "vut"
 
-Write-Host "DistDir: $DistDir"
-Write-Host "TargetDir: $TargetDir"
+# Get full version string
+$version = (Get-Content "version.json" | ConvertFrom-Json).FullVersion
 
-function Compress-Target([string] $ArchiveName, [string] $Target) {
-  Push-Location (Join-Path $TargetDir "$Target\release")
+function BuildTarget([string] $suffix, [string] $Target) {
+  $stagingPath = Join-Path $stagingRoot $Target
+
+  # Create staging directory
+  New-Item $stagingPath -ItemType Directory -Force
+
+  Cmd "Build $Target" { cargo build --release --target $Target }
+  Copy-Item "target\$Target\release\$executableName.exe" -Destination $stagingPath
+
+  # Create distribution archive
+  CreateArchive "$executableName-$version-$suffix" $stagingPath
+}
+
+<# Create distribution archive #>
+function CreateArchive([string] $ArchiveName, [string] $Path) {
+  Push-Location $Path
   try {
-    $ArchivePath = "$DistDir\$ArchiveName.zip"
+    $archivePath = "$distPath\$ArchiveName.zip"
 
     # Compress archive
-    7z a -mx9 $ArchivePath "vut.exe"
+    Cmd "Compressing archive" { 7z a -mx9 $archivePath * }
 
     # Generate checksum for archive
-    $checksum = (Get-FileHash -Path $ArchivePath -Algorithm SHA256).Hash.ToString()
-    Set-Content -Path "$ArchivePath.sha256.txt" -Encoding utf8NoBOM -Value $checksum -NoNewline
+    $checksum = (Get-FileHash -Path $archivePath -Algorithm SHA256).Hash.ToString()
+    Set-Content -Path "$archivePath.sha256.txt" -Encoding utf8NoBOM -Value $checksum -NoNewline
   } finally {
     Pop-Location
   }
 }
 
-# Delete old "dist" directory
-Write-Host "-- CLEAN --"
-Remove-Dir "$DistDir"
+try {
+  # Delete old output directories
+  Cmd "Clean" {
+    Remove-Dir $distPath
+    Remove-Dir $stagingRoot
+  }
 
-# Build executables
-Write-Host "-- BUILD --"
-cargo build --release --target i686-pc-windows-msvc
-cargo build --release --target x86_64-pc-windows-msvc
+  # Build and stage
+  BuildTarget "windows-i686" "i686-pc-windows-msvc"
+  BuildTarget "windows-x86_64" "x86_64-pc-windows-msvc"
 
-Write-Host "-- PACKAGE --"
+  # Create output directories
+  New-Item $distPath -ItemType Directory -Force
 
-# Create dist directory
-New-Item "$DistDir" -ItemType Directory -Force
+  # Write version file
+  Set-Content -Path "$distPath\VERSION.txt" -Encoding utf8NoBOM -Value $version -NoNewline
 
-# Construct path to newly built Vut executable
-$VutExe = Join-Path $TargetDir "x86_64-pc-windows-msvc\release\vut.exe"
+  # Build Chocolatey package
+  Cmd "Build Chocolatey package" { choco pack "chocolatey\package.nuspec" --outputdirectory $distPath --version=$version }
 
-# Get full version string
-$Version = (& $VutExe get json | ConvertFrom-Json).FullVersion
-
-# Create executable archives
-Compress-Target "vut-$Version-windows-i686" "i686-pc-windows-msvc"
-Compress-Target "vut-$Version-windows-x86_64" "x86_64-pc-windows-msvc"
-
-# Write version file
-Set-Content -Path "$DistDir\VERSION.txt" -Encoding utf8NoBOM -Value $Version -NoNewline
-
-# Build Chocolatey package
-choco pack "chocolatey\package.nuspec" --outputdirectory "$DistDir" --version=$Version
-
-# Remove version file after building Chocolatey package
-Remove-Item -Path "$DistDir\VERSION.txt"
+  # Remove version file after building Chocolatey package
+  Remove-Item -Path "$distPath\VERSION.txt"
+}
+catch {
+  Write-Host $_.Exception.Message -ForegroundColor Red
+}
